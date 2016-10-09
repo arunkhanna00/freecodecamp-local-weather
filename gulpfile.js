a364a214e493435d1b27259862f7e79820df4be9
Arun Khanna <arunkhanna00@gmail.com>
2016-10-09 13:48:53 -0700

    add a gulpfile.js file

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// Declare all modules required
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var rename = require('gulp-rename')
+var cssnano = require('gulp-cssnano');
+var htmlmin = require('gulp-htmlmin');
+var browserSync = require('browser-sync').create();
+
+// Minify Javascript
+gulp.task('minify-js', function() {
+    return gulp.src('app/index.js')
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(rename('index.min.js'))
+        .pipe(gulp.dest('dist/'))
+});
+
+// Minify CSS
+gulp.task('minify-css', function() {
+    return gulp.src('app/css/style.css')
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('dist/css/'))
+});
+
+// Minify HTML
+gulp.task('minify-html', function() {
+    return gulp.src('app/index.html')
+        .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true})))
+        .pipe(rename('index.min.html'))
+        .pipe(gulp.dest('dist/'))
+});
+
+// Sync with browser
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
+
+// Run gulp tasks whenever a file is saved
+gulp.task('watch', ['browserSync', 'minify-js', 'minify-css', 'minify-html'], function() {
+    gulp.watch('app/index.js', ['minify-js', browserSync.reload]);
+    gulp.watch('app/css/style.css', ['minify-css', browserSync.reload]);
+    gulp.watch('app/index.html', ['minify-html', browserSync.reload]);
+});
