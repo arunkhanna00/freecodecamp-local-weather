34ca5c10fdcfe4d0934a323e1be00c2f185648a1
Arun Khanna <arunkhanna00@gmail.com>
2016-11-11 13:54:14 -0800

    add angularjs

--- app/index.js
+++ app/index.js
@@ -1,9 +1,14 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    console.log("latitude: " + latitude + " longitude: " + longitude);
-  });
-}
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', ['$http', function($http) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $http.get("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=5f58edfe616a630892434c80c7630254")
+      .then(function(response){
+        console.log(response.data);
+      })
+    })
+  }
+}])
 
 
