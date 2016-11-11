2d11204614d7e4296a19a64ea98b38098f2ec55d
Arun Khanna <arunkhanna00@gmail.com>
2016-11-11 12:52:59 -0800

    find long and lat

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    console.log("latitude: " + latitude + " longitude: " + longitude);
+  });
+}
+
+
