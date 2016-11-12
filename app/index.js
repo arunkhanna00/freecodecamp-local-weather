6fbffd639894847377801895aa15bbd790e44f86
Arun Khanna <arunkhanna00@gmail.com>
2016-11-11 17:35:22 -0800

    add styling

--- app/index.js
+++ app/index.js
@@ -1,17 +1,21 @@
+// Setup module
 var app = angular.module('myApp', []);
 
+// Setup controller
 app.controller('myCtrl', ['$http', '$scope', function($http, $scope) {
+  // Find the geolocation coordinates if available
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
+      // Use AngularJs to request for the json of the OpenWeatherMap API
       $http.get("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=5f58edfe616a630892434c80c7630254")
+      // Store and display the data received
       .then(function(response){
-        console.log(response.data);
-        $scope.city = response.data.name;
-        $scope.icon = "http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png";
-        $scope.windSpeed = response.data.wind.speed;
+        $scope.city = response.data.name + ", " + response.data.sys.country;
+        $scope.icon = "https://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png";
+        $scope.windSpeed = Math.round(response.data.wind.speed / 0.44704); // convert from m/s to mi/hr
         $scope.humidity = response.data.main.humidity;
-        $scope.pressure = response.data.main.pressure;
-        $scope.temp = response.data.main.temp;
+        $scope.temp = Math.round(response.data.main.temp * (9 / 5) - 459.67); // convert from K to F
+        $scope.cloudiness = response.data.clouds.all;
       })
     })
   }
